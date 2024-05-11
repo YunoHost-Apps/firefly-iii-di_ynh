934ae3d2341a44601f77376bec44a8d639fc3c5f
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-05-11 12:33:33 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # PHP version required by FIII Data Importer
-YNH_PHP_VERSION="8.3"
+phpversion="8.3"
 
 # Composer version
 YNH_COMPOSER_VERSION="2.5.4"
